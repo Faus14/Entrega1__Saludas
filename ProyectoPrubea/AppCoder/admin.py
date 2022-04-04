73d0c821cc4d6aa8bdd85c5d7cbb64e6e252fb3f
Fausto Saludas <fausaludas14@gmail.com>
2022-04-03 22:21:06 -0300

    Agregamos la Herencia y creamos un user

--- ProyectoPrubea/AppCoder/admin.py
+++ ProyectoPrubea/AppCoder/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import persona
+from .models import persona,producto,horario
 
 # Register your models here.
 admin.site.register(persona)
+admin.site.register(producto)
+admin.site.register(horario)
